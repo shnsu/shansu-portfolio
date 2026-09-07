af624c3d0a49266ac8808cd017d6177bb333a548
Shansu <152450143+shnsu@users.noreply.github.com>
2026-09-07 11:31:16 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("menuBtn");
-const links = document.getElementById("navLinks");
-btn.addEventListener("click", () => {
-  links.classList.toggle("open");
-});
-links.querySelectorAll("a").forEach((a) => {
-  a.addEventListener("click", () => links.classList.remove("open"));
-});
